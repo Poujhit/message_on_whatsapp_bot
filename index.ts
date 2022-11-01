c04060ef0a481c2660f22d602ba6ab74600e74fb
Poujhit <poujhit.dev@gmail.com>
2022-11-01 22:02:37 +0530

    Some code added

--- index.ts
+++ index.ts
@@ -7,3 +7,9 @@ dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN as string);
 
 bot.start((ctx) => ctx.reply('Welcome lol'));
+
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
