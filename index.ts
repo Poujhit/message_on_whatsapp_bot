1f2ce87de1fdcc8093ec5932fff42f930e27273e
Poujhit <poujhit.dev@gmail.com>
2022-11-01 21:51:06 +0530

    Added some bot code

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-export {};
+import * as dotenv from 'dotenv';
+
+import { Telegraf } from 'telegraf';
+
+dotenv.config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN as string);
+
+bot.start((ctx) => ctx.reply('Welcome lol'));
