77710a3a9dc876961d9f0da4464a0ce800edce7c
Poujhit <poujhit.dev@gmail.com>
2022-12-03 23:05:22 +0530

    added code for the bot

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
 import * as dotenv from 'dotenv';
 
-import { Telegraf } from 'telegraf';
+import { Context, Telegraf } from 'telegraf';
+import { Update } from 'typegram';
 
 dotenv.config();
 
-const bot = new Telegraf(process.env.BOT_TOKEN as string);
+const bot: Telegraf<Context<Update>> = new Telegraf(
+  process.env.BOT_TOKEN as string
+);
 
 bot.start((ctx) => {
   console.log('here');
-  ctx.reply('Welcome this is cool');
+  ctx.reply(
+    'Welcome to this cool bot. Send a number to this bot and this bot will send you the whatsapp link to open that number in whatsapp. Currently works with indian numbers only'
+  );
+});
+
+bot.on('text', async (ctx) => {
+  console.log('HERE ON TEXT');
+  // Using context shortcut
+  await ctx.reply(`https://wa.me/91${ctx.message.text}`);
 });
 
 bot.launch();
